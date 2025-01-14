20ec19e8dab13bd900eed41c358cf9cec6d2972a
leonelcanelo <leonelcanelo218@gmail.com>
2025-01-14 01:03:51 -0300

    Primer entraga

---
+++ componentes/CartWidgetCss.jsx
@@ -0,0 +1,10 @@
+const CartWidgetCss = () => {
+    return(
+      <div className="CartWidget-container">
+        <img className="cart-img" src="../img/carro.jpeg" alt="carrito" />
+        <span className="cart-count">5</span>
+      </div>
+    )
+}
+
+export default CartWidgetCss
---
+++ componentes/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = (props) => {
+   // console.log (props)
+   // const {greeting}= props
+       return (
+         <div>
+             <h1>{props.greetig}</h1>
+         </div>
+  )    
+}
+export default ItemListContainer
